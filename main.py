0591d106265ae586c454987996e5542ca801f45c
fz3002 <franc3002@gmail.com>
2024-03-26 13:18:46 +0100

    File read

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import random
 
 class Perceptron:
@@ -10,25 +11,39 @@ class Perceptron:
 
     def set_weights(self, numberOfAttributes):
         for i in range(numberOfAttributes):
-            self.weights.append(random.randint(-5, 5)
+            self.weights.append(random.randint(-5, 5))
 
-    def Compute(self, vector):
+    def compute(self, vector):
         net = 0
         for i in range(len(vector)):
             net += (vector[i] * self.weights[i])
-        net -= alpha
+        net -= self.alpha
         if(net >= 0):
             return 1;
             
         return result
 
-    def Learn(self, prevResult, goodResult):
-        for weight in weights:
+    def learn(self, prevResult, goodResult):
+        for weight in self.weights:
             weight += (goodResult - prevResult) * self.alpha
 
     
     
 class Trainer:
+    def __init__(self, file_name, test_set_size):
+        self.file_name = file_name
+        
+    def read_file(file_name):
+        list = []
+        with open(file_name, newline='') as f:
+            lines = csv.reader(f, delimiter=";")
+        for row in lines:
+            list.append(row)
+        f.close()
+    
+    def create_sets(trains):
+        test_set_ids = []
+        for ()
 
 
 class UI:
